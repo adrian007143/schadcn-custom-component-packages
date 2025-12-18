c6f657408d403fd25ef4142f7aeaab293c29a207
adrian007143 <adrian.ced007@gmail.com>
2025-12-19 01:31:20 +0800

    update packages

---
+++ app/blocks/form-layout/page.tsx
@@ -0,0 +1,11 @@
+import FormLayoutComponent from "@/components/examples/form-layout-components";
+
+
+
+export default function TestPage() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen gap-4'>
+        <FormLayoutComponent />
+    </div>
+  )
+}
