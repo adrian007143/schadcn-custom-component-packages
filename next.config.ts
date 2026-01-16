e3dfb03c6fa2afc492ed8a6d8031784905c6b63a
adrian007143 <adrian.ced007@gmail.com>
2026-01-17 01:25:21 +0800

    update packages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: false,
+
+    qualities: [50, 75],
+
+    localPatterns: [
+      {
+        pathname: "/images/**",
+        search: undefined, // images without query params
+      },
+      {
+        pathname: "/images/**",
+        search: "(.*)", // images WITH any query params
+      },
+    ],
+  },
 };
 
 export default nextConfig;
